f89fe6ce271dae8fb5f1116cf16036d946c5df0b
ExtricateX <kevin.thet@gmail.com>
2015-02-10 23:48:46 -0800

    Database test
    Added my mongo driver, mongo access and interface.
    Also imported a java driver.

---
+++ app/src/main/java/com/example/andrew/comp3717project/MongoInterface.java
@@ -0,0 +1,6 @@
+package com.example.andrew.comp3717project;
+
+public interface MongoInterface {
+
+    public void processResult( String result );
+}
--- app/src/main/java/com/example/andrew/comp3717project/Register.java
+++ app/src/main/java/com/example/andrew/comp3717project/Register.java
@@ -1,20 +1,18 @@
 package com.example.andrew.comp3717project;
 
-import android.app.Activity;
 import android.content.Intent;
-
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 
 
-public class Register extends Activity {
+public class Register extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_register);
 
         Button register = (Button) findViewById(R.id.registerButton01);
@@ -23,6 +21,14 @@ public class Register extends Activity {
                 Intent registerIntent = new Intent(view.getContext(), Login.class);
                 startActivityForResult(registerIntent, 0);
                 setResult(RESULT_OK, registerIntent);
+                 String fname, lname, email, user, pass;
+                 fname = ((EditText)findViewById(R.id.firstName)).getText().toString();
+                 lname = ((EditText)findViewById(R.id.lastName)).getText().toString();
+                 user = ((EditText)findViewById(R.id.editText)).getText().toString();
+                pass = ((EditText)findViewById(R.id.editText2)).getText().toString();
+                 email = ((EditText)findViewById(R.id.editText3)).getText().toString();
+                MongoAccess mongoRegister = new MongoAccess();
+                 mongoRegister.pushUser(user,pass,email,fname,lname);
                 finish();
             }
 
