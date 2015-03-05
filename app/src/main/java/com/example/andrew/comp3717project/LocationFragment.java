99536be236f033839017427c0636af857f7e4fe5
duy159 <duyt.le@hotmail.ca>
2015-03-04 16:03:30 -0800

    added google maps. generated google api key

--- app/src/main/java/com/example/andrew/comp3717project/LocationFragment.java
+++ app/src/main/java/com/example/andrew/comp3717project/LocationFragment.java
@@ -6,14 +6,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.api.*;
+import com.google.android.gms.drive.*;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 /**
  * Created by Andrew on 1/21/2015.
  */
-public class LocationFragment extends Fragment{
+public class LocationFragment extends Fragment {
+    SupportMapFragment mapFragment;
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState )
     {
         // Inflate the layout for this fragment\
         return inflater.inflate( R.layout.fragment_location, container, false );
     }
+
+    /*
+    public void onCreate(Bundle savedInstanceState) {
+        mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0,0))
+                .title("Marker"));
+    }*/
 }
